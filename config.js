01bf11d56fab47dc50c138c4fdd742ac43ec4573
dimitris-kapoulas <618cbf0e9042d8297fa8649aa127e61e549d6833>
2021-01-03 22:09:54 +0200

    Social links fix

--- config.js
+++ config.js
@@ -134,10 +134,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/images/cto-ai.jpg',
-  //  pathPrefix: ` / $ {
-  //       REPOSITORY
-  //     }
-  //      `, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   firstName: FIRST_NAME,
   lastName: LAST_NAME,
   // bio
